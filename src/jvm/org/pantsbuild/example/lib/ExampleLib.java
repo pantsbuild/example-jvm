762f34763895d230be1c19094f0c36ebb3729670
Stu Hood <stuhood@gmail.com>
2022-01-10 23:27:58 -0800

    Bump to `2.9.0rc4`, and demonstrate using `resources`. (#3)

--- src/jvm/org/pantsbuild/example/lib/ExampleLib.java
+++ src/jvm/org/pantsbuild/example/lib/ExampleLib.java
@@ -1,9 +1,15 @@
 package org.pantsbuild.example.lib;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
+import com.google.common.io.Resources;
+import java.io.IOException;
 
 public class ExampleLib {
-  public static String hello() {
-    return Joiner.on(" ").join("Hello", "World!");
+  public static String hello() throws IOException {
+    String world =
+        Resources.toString(Resources.getResource(ExampleLib.class, "world.txt"), Charsets.UTF_8)
+            .strip();
+    return Joiner.on(" ").join("Hello", world);
   }
 }
